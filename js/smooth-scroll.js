3d1dae2a831f589a7a4ff7254d3d9b75a16fbfdd
Pwang-je <rewind1224@gmail.com>
2018-02-01 23:54:46 +0900

    5-1
    scroll to top, smooth scroll

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,14 +1,19 @@
-/*$("#About, #Skills, #Services, #Process, #Portf, #Contact").click(function() {
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1000);
-    return false;
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+    // top position relative to the document
+    var pos = $id.offset().top;
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos}, 800);
 });
 
-*/
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 600);
-    return false;
-});
+
+
